1afe405ee8466ca87706c6067b227a62ecd61203
zalmane <oren.elias@gmail.com>
2020-12-14 11:50:49 +0200

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="copybook",
-    version="1.0.3",
+    version="1.0.4",
     description="python copybook parser",
     long_description=README,
     
